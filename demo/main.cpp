9fd9cc5d7bc4f30cb0f90b82c21236e97899c4b0
sutsaiyi <sutsaiyi1@gmail.com>
2021-03-02 19:05:09 -0800

    add main.cpp

---
+++ demo/main.cpp
@@ -0,0 +1,24 @@
+#include "mbed.h"
+
+DigitalOut led3(LED3);
+DigitalOut led1(LED1);
+
+void Led(DigitalOut &ledpin, int i);
+
+int main(void)
+{
+	led3 = 1;
+	led1 = 1;
+	while(1) {
+		Led(led3, 6);
+		Led(led1, 4);
+	}
+}
+
+void Led(DigitalOut &ledpin, int times)
+{
+	for (int i = 0; i < times; i++) {
+		ledpin = !ledpin;
+		ThisThread::sleep_for(100ms);
+	}
+}
